6e8882fc029ab4b49825ae9ae02c6780a5ee8d10
emberone <71730998+emberone@users.noreply.github.com>
2025-11-08 23:24:25 +0300

    iter8

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"compress/gzip"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -11,6 +12,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -41,6 +43,16 @@ type request struct {
 
 type response struct {
 	URL string `json:"result"`
+}
+
+type gzipWriter struct {
+	http.ResponseWriter
+	Writer io.Writer
+}
+
+func (w gzipWriter) Write(b []byte) (int, error) {
+	// w.Writer будет отвечать за gzip-сжатие, поэтому пишем в него
+	return w.Writer.Write(b)
 }
 
 func main() {
@@ -75,7 +87,7 @@ func main() {
 	sugar = *logger.Sugar()
 
 	r := chi.NewRouter()
-	r.Use(WithLogging)
+	r.Use(WithLogging, gzipHandle)
 
 	r.Route(server.path, func(r chi.Router) {
 		r.Get("/{linkid}", getLinkHandler)
@@ -123,6 +135,40 @@ func WithLogging(h http.Handler) http.Handler {
 	}
 	// возвращаем функционально расширенный хендлер
 	return http.HandlerFunc(logFn)
+}
+
+func gzipHandle(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// проверяем, что клиент поддерживает gzip-сжатие
+		// это упрощённый пример. В реальном приложении следует проверять все
+		// значения r.Header.Values("Accept-Encoding") и разбирать строку
+		// на составные части, чтобы избежать неожиданных результатов
+		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			// если gzip не поддерживается, передаём управление
+			// дальше без изменений
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		mt, _, err := mime.ParseMediaType(r.Header.Get("Content-Type"))
+		if err != nil || (mt != "application/json" && mt != "text/html") {
+			next.ServeHTTP(w, r)
+			return
+
+		}
+
+		// создаём gzip.Writer поверх текущего w
+		gz, err := gzip.NewWriterLevel(w, gzip.BestSpeed)
+		if err != nil {
+			io.WriteString(w, err.Error())
+			return
+		}
+		defer gz.Close()
+
+		w.Header().Set("Content-Encoding", "gzip")
+		// передаём обработчику страницы переменную типа gzipWriter для вывода данных
+		next.ServeHTTP(gzipWriter{ResponseWriter: w, Writer: gz}, r)
+	})
 }
 
 func getLinkHandler(w http.ResponseWriter, r *http.Request) {
